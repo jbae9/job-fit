f09f631500ed590b9abf02a6a1621ee459f02b36
Jin Bae 배진영 <jinbae00@gmail.com>
2023-02-28 11:26:39 +0900

    refractor, build: .entity.ts 파일들을 entity 폴더로 옮김, job-post 클래스와 파일명를 jobpost으로 수정, jobpost.entity 칼럼 수정

--- src/_tests/job-post.service.spec.ts
+++ src/_tests/job-post.service.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing'
-import { JobPostService } from './job-post.service'
+import { JobpostService } from '../jobpost/jobpost.service'
 
-describe('JobPostService', () => {
-    let service: JobPostService
+describe('JobpostService', () => {
+    let service: JobpostService
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [JobPostService],
+            providers: [JobpostService],
         }).compile()
 
-        service = module.get<JobPostService>(JobPostService)
+        service = module.get<JobpostService>(JobpostService)
     })
 
     it('should be defined', () => {
