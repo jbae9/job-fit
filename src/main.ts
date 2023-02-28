25bf4fbaab14f7c5746510ae83fc74baab012199
ParkAsher <devasherpark@naver.com>
2023-02-28 18:24:44 +0900

    feat: hbs세팅과 index 페이지 테스트

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core'
+import { NestExpressApplication } from '@nestjs/platform-express'
+import { join } from 'path'
 import { AppModule } from './app.module'
 
 declare const module: any
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule)
+    const app = await NestFactory.create<NestExpressApplication>(AppModule)
+
+    // hbs 세팅
+    app.useStaticAssets(join(__dirname, '..', 'public'))
+    app.setBaseViewsDir(join(__dirname, '..', 'views'))
+    app.setViewEngine('hbs')
+
     await app.listen(process.env.PORT)
 
     if (module.hot) {
