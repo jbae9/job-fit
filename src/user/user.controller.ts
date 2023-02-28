ffe46bc8609fc5e4cdd55ed74ef840b6872c623d
ParkAsher <devasherpark@naver.com>
2023-03-01 00:17:41 +0900

    feat: JWT Auth Guard 테스트를 위한 프론트 구현

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,31 @@
 import { Controller } from '@nestjs/common'
+import { Get, Req, Res, UseGuards } from '@nestjs/common/decorators'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { KakaoAuthGuard } from 'src/auth/kakao-auth.guard'
+import { UserService } from './user.service'
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService) {}
+
+    @UseGuards(KakaoAuthGuard)
+    @Get('/auth/kakao')
+    async kakaoLogin() {}
+
+    @UseGuards(KakaoAuthGuard)
+    @Get('/auth/kakao/callback')
+    async kakaoCallback(@Req() req, @Res() res) {
+        const { userAccessToken, userRefreshToken } = req.user
+
+        res.cookie('accessToken', userAccessToken)
+        res.cookie('refreshToken', userRefreshToken)
+
+        return res.redirect('/')
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('/auth/test')
+    async test(@Req() req) {
+        return req.user
+    }
+}
