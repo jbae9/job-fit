469ffda5fde312a9af1bfaf0f7e67c80b2eaeb99
ParkAsher <devasherpark@naver.com>
2023-02-28 11:25:04 +0900

    fix: 회원 entity 수정

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -14,14 +14,23 @@ export class User {
     userId: number
 
     @Index({ unique: true })
-    @Column()
+    @Column({ type: 'varchar' })
     email: string
 
-    @Column('varchar', { length: 200, select: false })
-    password: string
+    @Column({ type: 'varchar' })
+    nickname: string | null
 
-    @Column('varchar', { length: 50 })
+    @Column({ type: 'varchar' })
+    profileImage: string | null
+
+    @Column({ type: 'varchar' })
     role: string
+
+    @Column({ type: 'varchar' })
+    addressUpper: string | null
+
+    @Column({ type: 'varchar' })
+    addressLower: string | null
 
     @CreateDateColumn()
     createdDtm: Date
