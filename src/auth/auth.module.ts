8500d37a0cf17717847927f458a88c00f6080446
ParkAsher <devasherpark@naver.com>
2023-03-02 11:57:10 +0900

    feat: User Controller 에서 Auth Module를 사용하기 위해 export

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common'
+import { CacheModule, Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { JwtModule } from '@nestjs/jwt'
 import { PassportModule } from '@nestjs/passport'
@@ -6,21 +6,34 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { User } from 'src/entities/user.entity'
 import { JwtConfigService } from 'src/_config/jwt.config.service'
 import { AuthService } from './auth.service'
-import { JwtStrategy } from './jwt.strategy'
-import { KakaoStrategy } from './kakao.straregy'
+import { JwtStrategy } from './strategy/jwt.strategy'
+import { KakaoStrategy } from './strategy/kakao.straregy'
+import redisStore from 'cache-manager-redis-store'
 
 @Module({
     imports: [
         PassportModule.register({
             session: false,
         }),
-        TypeOrmModule.forFeature([User]), // 이건 TypeORM 강의 시간에 배웠죠?
+        TypeOrmModule.forFeature([User]),
         JwtModule.registerAsync({
             imports: [ConfigModule],
             useClass: JwtConfigService,
             inject: [ConfigService],
         }),
+        CacheModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                isGlobal: true,
+                store: redisStore,
+                host: configService.get('REDIS_HOST'),
+                port: Number(configService.get('REDIS_PORT')),
+                password: configService.get('REDIS_PASSWORD'),
+            }),
+        }),
     ],
     providers: [AuthService, KakaoStrategy, JwtStrategy],
+    exports: [PassportModule, JwtModule, AuthService],
 })
 export class AuthModule {}
