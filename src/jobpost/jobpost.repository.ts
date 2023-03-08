23146e589004aa3172e0b7f042867564057bf348
ParkAsher <devasherpark@naver.com>
2023-03-08 14:26:42 +0900

    fix: 채용공고 DB저장 쿼리 수정

--- src/jobpost/jobpost.repository.ts
+++ src/jobpost/jobpost.repository.ts
@@ -5,19 +5,13 @@ import { CompanyRepository } from 'src/company/company.repository'
 
 @Injectable()
 export class JobpostRepository extends Repository<Jobpost> {
-    constructor(private dataSource: DataSource,
-        private companyRepository: CompanyRepository) {
+    constructor(
+        private dataSource: DataSource,
+        private companyRepository: CompanyRepository
+    ) {
         super(Jobpost, dataSource.createEntityManager())
     }
 
-
-    async postJobpostsInBulk(jobposts) {
-        await this.createQueryBuilder()
-            .insert()
-            .into(Jobpost)
-            .values(jobposts)
-            .execute()
-    }
     async createJobposts(jobposts) {
         // 공고 데이터 한번씩 돌면서
         for (let jobpost of jobposts) {
@@ -31,44 +25,43 @@ export class JobpostRepository extends Repository<Jobpost> {
                 originalImgUrl,
                 postedDtm,
                 deadlineDtm,
+                originalAddress,
+                addressUpper,
+                addressLower,
+                longitude,
+                latitude,
             } = jobpost
 
-            const companyId = await this.companyRepository.findCompanyId(companyName)
+            // 회사 id 들고오는 쿼리
+            const companyId = await this.companyRepository.findCompanyId(
+                companyName
+            )
 
-            const query = `INSERT INTO jobpost (company_id, title, content, salary, original_site_name, original_url, original_img_url, posted_dtm, deadline_dtm)
-                           VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-                           ON DUPLICATE KEY UPDATE
-                           title = COALESCE(?, title),
-                           content = COALESCE(?, content),
-                           salary = COALESCE(?, salary),
-                           original_site_name = COALESCE(?, original_site_name),
-                           original_url = COALESCE(?, original_url),
-                           original_img_url = COALESCE(?, original_img_url),
-                           posted_dtm = COALESCE(?, posted_dtm),
-                           deadline_dtm = COALESCE(?, deadline_dtm)`
-
-
-            const values = [
-                companyId,
-                title,
-                content,
-                salary,
-                originalSiteName,
-                originalUrl,
-                originalImgUrl,
-                postedDtm,
-                deadlineDtm,
-                title,
-                content,
-                salary,
-                originalSiteName,
-                originalUrl,
-                originalImgUrl,
-                postedDtm,
-                deadlineDtm,
-            ]
-
-            await this.query(query, values)
+            await this.createQueryBuilder('jobpost')
+                .insert()
+                .into('jobpost')
+                .values({
+                    companyId,
+                    title,
+                    content,
+                    salary,
+                    originalSiteName,
+                    originalUrl,
+                    originalImgUrl,
+                    postedDtm,
+                    deadlineDtm,
+                    originalAddress,
+                    addressUpper,
+                    addressLower,
+                    longitude,
+                    latitude,
+                })
+                .orUpdate(
+                    ['salary', 'original_img_url', 'deadline_dtm'],
+                    ['company_id', 'title']
+                )
+                .updateEntity(false)
+                .execute()
         }
     }
 }
