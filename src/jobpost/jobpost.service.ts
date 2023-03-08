d4610bc387bc04eed267a213bb0a07c31fa8664b
ParkAsher <devasherpark@naver.com>
2023-03-08 14:35:26 +0900

    fix: 채용공고 관련 함수명 수정 및 통일

--- src/jobpost/jobpost.service.ts
+++ src/jobpost/jobpost.service.ts
@@ -1,9 +1,9 @@
 import { Injectable, Logger } from '@nestjs/common'
 import { CompanyRepository } from 'src/company/company.repository'
 import { JobpostRepository } from './jobpost.repository'
-import { wantedScraper } from './jobpostWantedAxiosScraper'
-import { SaraminSelenium } from './jobpostSaraminSeleniumScraper'
-import { programmersScraper } from './jobpostProgrammersScraper'
+import { wantedScraper } from './scraper/jobpostWantedAxiosScraper'
+import { SaraminSelenium } from './scraper/jobpostSaraminSeleniumScraper'
+import { programmersScraper } from './scraper/jobpostProgrammersScraper'
 
 @Injectable()
 export class JobpostService {
@@ -11,65 +11,36 @@ export class JobpostService {
         private jobpostRepository: JobpostRepository,
         private companyRepository: CompanyRepository,
         private logger: Logger
-    ) { }
+    ) {}
 
-    async postSaraminJobposts() {
+    async getSaraminJobposts() {
         const saraminScraper = new SaraminSelenium('10')
         const { companies, jobposts } = await saraminScraper.getSaraminScraper()
+
+        // 회사 데이터 넣기
         await this.companyRepository.createCompanies(companies)
+
+        // 채용공고 데이터 넣기
         await this.jobpostRepository.createJobposts(jobposts)
-        this.logger.log('사람인 크롤링 작업 완료')
     }
 
-    async postWantedJobposts() {
+    async getWantedJobposts() {
         const { companies, jobposts } = await wantedScraper()
-        await this.companyRepository.save(companies)
 
-        for (let i = 0; i < jobposts.length; i++) {
-            const companyId = await this.companyRepository.find({
-                select: { companyId: true },
-                where: { companyName: jobposts[i].companyName },
-            })
-            this.logger.log(companyId)
+        // 회사 데이터 넣기
+        await this.companyRepository.createCompanies(companies)
 
-            await this.jobpostRepository.save({
-                companyId: companyId[0].companyId,
-                title: jobposts[i].title,
-                content: jobposts[i].content,
-                salary: jobposts[i].salary,
-                originalSiteName: jobposts[i].originalSiteName,
-                originalUrl: jobposts[i].originalUrl,
-                originalImgUrl: jobposts[i].originalImgUrl,
-                postedDtm: jobposts[i].postedDtm,
-                deadlineDtm: jobposts[i].deadlineDtm,
-            })
-        }
+        // 채용공고 데이터 넣기
+        await this.jobpostRepository.createJobposts(jobposts)
     }
 
-    async createProgrammersJobposts() {
+    async getProgrammersJobposts() {
         const { jobposts, companiesSetArray } = await programmersScraper()
 
         // 회사 데이터 넣기
         await this.companyRepository.createCompanies(companiesSetArray)
 
         // 채용공고 데이터 넣기
-        for (let i = 0; i < jobposts.length; i++) {
-            const companyId = await this.companyRepository.find({
-                where: { companyName: jobposts[i].companyName },
-                select: { companyId: true },
-            })
-
-            await this.jobpostRepository.save({
-                companyId: companyId[0].companyId,
-                title: jobposts[i].title,
-                content: jobposts[i].content,
-                salary: jobposts[i].salary,
-                originalSiteName: jobposts[i].originalSiteName,
-                originalUrl: jobposts[i].originalUrl,
-                originalImgUrl: jobposts[i].originalImgUrl,
-                postedDtm: jobposts[i].postedDtm,
-                deadlineDtm: jobposts[i].deadlineDtm,
-            })
-        }
+        await this.jobpostRepository.createJobposts(jobposts)
     }
 }
