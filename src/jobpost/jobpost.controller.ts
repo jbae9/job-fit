de7f64a6bafebd0ddd6ee76c0e2d3996552bf848
Jin Bae 배진영 <jinbae00@gmail.com>
2023-03-09 13:34:49 +0900

    chore: 머지 전 커밋

--- src/jobpost/jobpost.controller.ts
+++ src/jobpost/jobpost.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Get, Query } from '@nestjs/common'
+import { Controller, Get, Query } from '@nestjs/common'
 import { JobpostService } from './jobpost.service'
 
 @Controller('api/jobpost')
