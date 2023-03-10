103ffe320e8119b694ec631cabebed6e3b94cf2c
ParkAsher <devasherpark@naver.com>
2023-03-10 19:00:05 +0900

    feat: 마이페이지 내정보 관리 라우터

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -54,11 +54,10 @@ export class AppController {
             ? null
             : req.authResult.user
 
-        if (user) return res.redirect('/')
-
         return { components: 'jobposts', user: user }
     }
 
+    // 마이페이지 - 기본은 내 정보 카테고리
     @UseGuards(JwtAuthGuard)
     @Get('/mypage')
     @Render('index')
@@ -73,6 +72,24 @@ export class AppController {
                 href: '/',
             })
 
-        return { components: 'mypage', user: user }
+        return { components: 'mypage', user: user, subComponents: 'myinfo' }
+    }
+
+    // 마이페이지 내 정보 카테고리
+    @UseGuards(JwtAuthGuard)
+    @Get('/mypage/myinfo')
+    @Render('index')
+    mypageMyinfo(@Req() req, @Res() res) {
+        const user = !req.authResult.hasOwnProperty('user')
+            ? null
+            : req.authResult.user
+
+        if (!user)
+            return res.render('alert.ejs', {
+                message: '로그인이 필요합니다.',
+                href: '/',
+            })
+
+        return { components: 'mypage', user: user, subComponents: 'myinfo' }
     }
 }
