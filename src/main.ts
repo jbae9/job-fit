42ad177a1c10471966a6160b70bd53c561f44dde
ParkAsher <devasherpark@naver.com>
2023-03-12 23:31:29 +0900

    feat: globalpipe 설정

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { NestExpressApplication } from '@nestjs/platform-express'
 import { join } from 'path'
 import { AppModule } from './app.module'
 import cookieParser from 'cookie-parser'
+import { ValidationPipe } from '@nestjs/common'
 
 declare const module: any
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule)
 
+    app.useGlobalPipes(new ValidationPipe({ transform: true }))
     app.use(cookieParser())
 
     // ejs 세팅
