bb7bb549455e3f1d5812e4903b1cb56ec4988189
ParkAsher <devasherpark@naver.com>
2023-03-23 22:12:18 +0900

    refactor: AWS ElastiCache Redis Cluster URL env setting

--- src/_config/cache.config.ts
+++ src/_config/cache.config.ts
@@ -12,7 +12,7 @@ export class RedisConfigService implements RedisOptionsFactory {
     async createRedisOptions(): Promise<RedisModuleOptions> {
         return {
             config: {
-                url: this.configService.get('REDIS_URL'),
+                url: 'redis://jobfit-redis.usn2ar.ng.0001.apn2.cache.amazonaws.com:6379',
             },
         }
     }
