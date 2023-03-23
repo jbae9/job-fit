20f27a1527db70c3167058607572084e56747f84
Jin Bae 배진영 <jinbae00@gmail.com>
2023-03-23 18:51:25 +0900

    refactor: chromedriver 경로 수정

--- src/jobpost/scraper/jobpostSaraminSeleniumScraper.ts
+++ src/jobpost/scraper/jobpostSaraminSeleniumScraper.ts
@@ -37,9 +37,7 @@ export class SaraminSelenium {
         const driver = await new Builder()
             .forBrowser('chrome')
             .setChromeOptions(options)
-            .setChromeService(
-                new chrome.ServiceBuilder('../../../chromedriver')
-            )
+            .setChromeService(new chrome.ServiceBuilder('../chromedriver'))
             .build()
         while (true) {
             console.log(`사람인 ${page}페이지 스크롤 중...`)
