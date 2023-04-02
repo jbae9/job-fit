5921dc8bf28f464f1487301747faa7d096dfbee6
ParkAsher <devasherpark@naver.com>
2023-04-02 17:50:49 +0900

    refactor: 채용공고 크롤링 관련 함수 주석처리

--- src/jobpost/jobpost.controller.ts
+++ src/jobpost/jobpost.controller.ts
@@ -15,22 +15,22 @@ export class JobpostController {
     constructor(private readonly jobpostService: JobpostService) {}
 
     // @Cron('0 0 5 * * *') // 매일 새벽 5시
-    @Get('/saramin')
-    async getSaraminData() {
-        return await this.jobpostService.getSaraminJobposts()
-    }
+    // @Get('/saramin')
+    // async getSaraminData() {
+    //     return await this.jobpostService.getSaraminJobposts()
+    // }
 
     // @Cron('0 0 4 * * *') // 매일 새벽 4시
-    @Get('/wanted')
-    async getWantedJobposts() {
-        return await this.jobpostService.getWantedJobposts()
-    }
+    // @Get('/wanted')
+    // async getWantedJobposts() {
+    //     return await this.jobpostService.getWantedJobposts()
+    // }
 
     // @Cron('0 0 3 * * *') // 매일 새벽 3시
-    @Get('/programmers')
-    async getProgrammersJobposts() {
-        return await this.jobpostService.getProgrammersJobposts()
-    }
+    // @Get('/programmers')
+    // async getProgrammersJobposts() {
+    //     return await this.jobpostService.getProgrammersJobposts()
+    // }
 
     @Get('/filter/:userId')
     async getRecommendedJobposts(
