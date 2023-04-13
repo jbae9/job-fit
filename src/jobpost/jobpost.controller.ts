4953176108de4245215a93399b015178dd354982
Jin Bae 배진영 <jinbae00@gmail.com>
2023-04-13 21:58:31 +0900

    refactor: Redis config 수정, cron job UTC 시간으로 수정, 원티드와 프로그래머스 스크레이퍼를 동기적으로 작동하게 수정

--- src/jobpost/jobpost.controller.ts
+++ src/jobpost/jobpost.controller.ts
@@ -14,19 +14,19 @@ import { JobpostService } from './jobpost.service'
 export class JobpostController {
     constructor(private readonly jobpostService: JobpostService) {}
 
-    // @Cron('0 0 5 * * *') // 매일 새벽 5시
+    // @Cron('0 0 14 * * *') // 매일 새벽 5시
     @Get('/saramin')
     async getSaraminData() {
         return await this.jobpostService.getSaraminJobposts()
     }
 
-    @Cron('0 0 4 * * *') // 매일 새벽 4시
+    @Cron('0 0 13 * * *') // 매일 새벽 4시 (UTC+0 13시)
     @Get('/wanted')
     async getWantedJobposts() {
-        return await this.jobpostService.getWantedJobposts()
+        return this.jobpostService.getWantedJobposts()
     }
 
-    @Cron('0 0 3 * * *') // 매일 새벽 3시
+    @Cron('0 0 12 * * *') // 매일 새벽 3시 (UTC+0 12시)
     @Get('/programmers')
     async getProgrammersJobposts() {
         return await this.jobpostService.getProgrammersJobposts()
--- src/jobpost/jobpost.service.ts
+++ src/jobpost/jobpost.service.ts
@@ -1,11 +1,10 @@
 import { Injectable, Logger } from '@nestjs/common'
 import { CompanyRepository } from 'src/company/company.repository'
 import { JobpostRepository } from './jobpost.repository'
-import { wantedScraper } from './scraper/jobpostWantedAxiosScraper'
+import { WantedScraper } from './scraper/jobpostWantedAxiosScraper'
 import { SaraminSelenium } from './scraper/jobpostSaraminSeleniumScraper'
-import { programmersScraper } from './scraper/jobpostProgrammersScraper'
+import { ProgrammersScraper } from './scraper/jobpostProgrammersScraper'
 import { CacheService } from 'src/cache/cache.service'
-import { Cron } from '@nestjs/schedule'
 
 @Injectable()
 export class JobpostService {
@@ -24,23 +23,21 @@ export class JobpostService {
     }
 
     async getWantedJobposts() {
-        const { companies, jobposts } = await wantedScraper()
+        const wantedScraper = new WantedScraper(
+            this.companyRepository,
+            this.jobpostRepository
+        )
 
-        // 회사 데이터 넣기
-        await this.companyRepository.createCompanies(companies)
-
-        // 채용공고 데이터 넣기
-        await this.jobpostRepository.createJobposts(jobposts)
+        wantedScraper.wantedScraper()
     }
 
     async getProgrammersJobposts() {
-        const { jobposts, companiesSetArray } = await programmersScraper()
+        const programmersScraper = new ProgrammersScraper(
+            this.companyRepository,
+            this.jobpostRepository
+        )
 
-        // 회사 데이터 넣기
-        await this.companyRepository.createCompanies(companiesSetArray)
-
-        // 채용공고 데이터 넣기
-        await this.jobpostRepository.createJobposts(jobposts)
+        programmersScraper.programmersScraper()
     }
 
     async getFilteredJobposts(query: {
